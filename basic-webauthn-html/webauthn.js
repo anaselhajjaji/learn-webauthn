e5ca6767ff12d5e18dc90046cb3584d3a5295ea4
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-07-28 09:20:01 -0400

    updates documentation

--- basic-webauthn-html/webauthn.js
+++ basic-webauthn-html/webauthn.js
@@ -1,24 +1,23 @@
 // STEP 1
-// The server would begin creating a new credential by calling 
-// navigator.credentials.create() on the client
+// In this step, the server will create a new credential by calling navigator.credentials.create() on the client side
 async function registerCredentials() {
     var randomStringFromServer = 'I am a random string generated from server';
     
     const publicKeyCredentialCreationOptions = {
         // challenge: The challenge is a buffer of cryptographically random bytes generated on the server, and is needed to prevent "replay attacks". 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-challenge
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-challenge
         challenge: Uint8Array.from(
             randomStringFromServer, c => c.charCodeAt(0)),
         // rp: This stands for “relying party”; it can be considered as describing the organization responsible for registering and authenticating the user. 
         // The id must be a subset of the domain currently in the browser. For example, a valid id for this page is webauthn.guide. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-rp
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-rp
         rp: {
             name: "Anas Localhost",
             id: "localhost", // To make the example work on localhost, otherwise it should be: something.com
         },
         // user: This is information about the user currently registering. The authenticator uses the id to associate a credential with the user. 
         // It is suggested to not use personally identifying information as the id, as it may be stored in an authenticator. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-user
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-user
         user: {
             id: Uint8Array.from(
                 "THE_ID_OF_ANAS_USER", c => c.charCodeAt(0)),
@@ -28,25 +27,25 @@ async function registerCredentials() {
         // pubKeyCredParams: This is an array of objects describing what public key types are acceptable to a server. 
         // The alg is a number described in the COSE registry; for example, -7 indicates that the server accepts Elliptic Curve public keys 
         // using a SHA-256 signature algorithm. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-pubkeycredparams
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-pubkeycredparams
         pubKeyCredParams: [{alg: -7, type: "public-key"},{alg: -257, type: "public-key"}],
         // authenticatorSelection: This optional object helps relying parties make further restrictions on the type of authenticators allowed for registration. 
         // If we use "cross-platform" it'll indicate that we want to register a cross-platform authenticator (like a Yubikey) instead of a platform authenticator like Windows Hello or Touch ID. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-authenticatorselection
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-authenticatorselection
         authenticatorSelection: {
             authenticatorAttachment: 'platform',
             userVerification: 'required'
         },
         excludeCredentials: [],
         // timeout: The time (in milliseconds) that the user has to respond to a prompt for registration before an error is returned. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-timeout
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-timeout
         timeout: 60000,
         // attestation: The attestation data that is returned from the authenticator has information that could be used to track users. 
         // This option allows servers to indicate how important the attestation data is to this registration event. 
         // A value of "none" indicates that the server does not care about attestation. 
         // A value of "indirect" means that the server will allow for anonymized attestation data. 
         // direct means that the server wishes to receive the attestation data from the authenticator. 
-        // Read the spec: https://w3c.github.io/webauthn/#attestation-conveyance
+        // Spec Documentation: https://w3c.github.io/webauthn/#attestation-conveyance
         attestation: "none"
     };
     
@@ -55,19 +54,19 @@ async function registerCredentials() {
         PublicKeyCredential {
             // id: The ID for the newly generated credential; 
             // it will be used to identify the credential when authenticating the user. 
-            // The ID is provided here as a base64-encoded string. Read the spec: https://w3c.github.io/webauthn/#ref-for-dom-credential-id
+            // The ID is provided here as a base64-encoded string. Spec Documentation: https://w3c.github.io/webauthn/#ref-for-dom-credential-id
             id: 'ADSUllKQmbqdGtpu4sjseh4cg2TxSvrbcHDTBsv4NSSX9...',
             // rawId: The ID again, but in binary form. 
-            // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredential-rawid
+            // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredential-rawid
             rawId: ArrayBuffer(59),
             response: AuthenticatorAttestationResponse {
                 // clientDataJSON: This represents data passed from the browser to the authenticator in order to associate the new credential with the server and browser. 
                 // The authenticator provides it as a UTF-8 byte array. 
-                // Read the spec: https://w3c.github.io/webauthn/#dictdef-collectedclientdata
+                // Spec Documentation: https://w3c.github.io/webauthn/#dictdef-collectedclientdata
                 clientDataJSON: ArrayBuffer(121),
                 // attestationObject: This object contains the credential public key, an optional attestation certificate, 
                 // and other metadata used also to validate the registration event. It is binary data encoded in CBOR. 
-                // Read the spec: https://w3c.github.io/webauthn/#dom-authenticatorattestationresponse-attestationobject
+                // Spec Documentation: https://w3c.github.io/webauthn/#dom-authenticatorattestationresponse-attestationobject
                 attestationObject: ArrayBuffer(306),
             },
             type: 'public-key'
@@ -114,17 +113,17 @@ async function registerCredentials() {
         {
             // authData: The authenticator data is here is a byte array that contains metadata about the registration event, 
             // as well as the public key we will use for future authentications. 
-            // Read the spec: https://w3c.github.io/webauthn/#authenticator-data
+            // Spec Documentation: https://w3c.github.io/webauthn/#authenticator-data
             authData: Uint8Array(196),
             // fmt: This represents the attestation format. Authenticators can provide attestation data in a number of ways; 
             // this indicates how the server should parse and validate the attestation data. 
-            // Read the spec: https://w3c.github.io/webauthn/#attestation-statement-format
+            // Spec Documentation: https://w3c.github.io/webauthn/#attestation-statement-format
             fmt: "fido-u2f",
             // attStmt: This is the attestation statement. This object will look different depending on the attestation format indicated. 
             // In this case, we are given a signature sig and attestation certificate x5c. 
             // Servers use this data to cryptographically verify the credential public key came from the authenticator. 
             // Additionally, servers can use the certificate to reject authenticators that are believed to be weak. 
-            // Read the spec: https://w3c.github.io/webauthn/#attestation-statement
+            // Spec Documentation: https://w3c.github.io/webauthn/#attestation-statement
             attStmt: {
                 sig: Uint8Array(70),
                 x5c: Array(1),
@@ -201,13 +200,13 @@ async function authenticate() {
         },
         rpId: "localhost", // To make the example work on localhost, otherwise it should be: something.com
         // challenge: Like during registration, this must be cryptographically random bytes generated on the server. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialrequestoptions-challenge
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialrequestoptions-challenge
         challenge: Uint8Array.from(
             randomStringFromServer, c => c.charCodeAt(0)),
         // allowCredentials: This array tells the browser which credentials the server would like the user to authenticate with. 
         // The credentialId retrieved and saved during registration is passed in here. The server can optionally indicate what transports it prefers, 
         // like USB, NFC, and Bluetooth. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialrequestoptions-allowcredentials
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialrequestoptions-allowcredentials
         allowCredentials: [{
             id: base64DecodeURL(credentialId),
             type: 'public-key',
@@ -216,7 +215,7 @@ async function authenticate() {
         }],
         userVerification: 'required',
         // timeout: Like during registration, this optionally indicates the time (in milliseconds) that the user has to respond to a prompt for authentication. 
-        // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredentialrequestoptions-timeout
+        // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredentialrequestoptions-timeout
         timeout: 60000
     }
     
@@ -231,28 +230,28 @@ async function authenticate() {
     /*
         PublicKeyCredential {
             // id: The identifier for the credential that was used to generate the authentication assertion. 
-            // Read the spec: https://w3c.github.io/webauthn/#ref-for-dom-credential-id
+            // Spec Documentation: https://w3c.github.io/webauthn/#ref-for-dom-credential-id
             id: 'ADSUllKQmbqdGtpu4sjseh4cg2TxSvrbcHDTBsv4NSSX9...',
             // rawId: The identifier again, but in binary form. 
-            // Read the spec: https://w3c.github.io/webauthn/#dom-publickeycredential-rawid
+            // Spec Documentation: https://w3c.github.io/webauthn/#dom-publickeycredential-rawid
             rawId: ArrayBuffer(59),
             response: AuthenticatorAssertionResponse {
                 // authenticatorData: The authenticator data is similar to the authData received during registration, 
                 // with the notable exception that the public key is not included here. It is another item used during authentication 
                 // as source bytes to generate the assertion signature. 
-                // Read the spec: https://w3c.github.io/webauthn/#authenticator-data
+                // Spec Documentation: https://w3c.github.io/webauthn/#authenticator-data
                 authenticatorData: ArrayBuffer(191),
                 // clientDataJSON: As during registration, the clientDataJSON is a collection of the data passed from the browser to the authenticator. 
                 // It is one of the items used during authentication as the source bytes to generate the signature. 
-                // Read the spec: https://w3c.github.io/webauthn/#dictdef-collectedclientdata
+                // Spec Documentation: https://w3c.github.io/webauthn/#dictdef-collectedclientdata
                 clientDataJSON: ArrayBuffer(118),
                 // signature: The signature generated by the private key associated with this credential. On the server, 
                 // the public key will be used to verify that this signature is valid. 
-                // Read the spec: https://w3c.github.io/webauthn/#dom-authenticatorassertionresponse-signature
+                // Spec Documentation: https://w3c.github.io/webauthn/#dom-authenticatorassertionresponse-signature
                 signature: ArrayBuffer(70),
                 // userHandle: This field is optionally provided by the authenticator, and represents the user.id that was supplied during registration. 
                 // It can be used to relate this assertion to the user on the server. It is encoded here as a UTF-8 byte array. 
-                // Read the spec: https://w3c.github.io/webauthn/#dom-authenticatorassertionresponse-userhandle
+                // Spec Documentation: https://w3c.github.io/webauthn/#dom-authenticatorassertionresponse-userhandle
                 userHandle: ArrayBuffer(10),
             },
             type: 'public-key'
